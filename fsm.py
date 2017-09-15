ca7b5fdb8ad6548cd4f74be91e2b439aa9142a2b
Alex Maeda <amaeda@uber.com>
2017-09-15 00:02:05 -0700

    reorganized constants

--- fsm.py
+++ fsm.py
@@ -1,45 +1,39 @@
 import pygame
 import random
-from math import pi
+import math
 
-background_colour = (255,255,255)
-(width, height) = (800, 600)
+#CONSTANTS
+BACKGROUND_COLOR = (255,255,255)
+(WIDTH, HEIGHT) = (800, 600)
 
-#colors
-black = (0,0,0)
-green = (200,0,0)
-red = (0,255,0)
+BLACK = (0,0,0)
+GREEN = (200,0,0)
+RED = (0,255,0)
 
-ivory = (238,233,233)
-
-screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption('Tutorial 1')
-screen.fill(background_colour)
-
-pygame.display.flip()
-clock = pygame.time.Clock()
-
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 circleCounter = 0
 
 #position of the first circle
 centerX = 200
 centerY = 200
 
-face = pygame.Surface((5, 5))
-
-running = True
 def circleButton(cx, cy):
+    global screen
     mouse = pygame.mouse.get_pos()
     click = pygame.mouse.get_pressed()
+
     radius = cx + 40
     print(radius)
+
     px = mouse[0]
     py = mouse[1]
     print(px, py)
+
     pythag = math.sqrt(math.pow((px - cx), 2) + math.pow((py - cy), 2))
     print(pythag)
-    if click[0] == 1 and ivory:
-      pygame.draw.circle(screen, black, (cx, cy), 40)
+
+    if click[0] == 1:
+      pygame.draw.circle(screen, BLACK, (cx, cy), 40)
 
 def button(msg, x, y, w, h, ic, ac, action=None):
   mouse = pygame.mouse.get_pos()
@@ -52,11 +46,11 @@ def button(msg, x, y, w, h, ic, ac, action=None):
     if click[0] == 1 and action != None:
       if action == "Circle":
         if circleCounter == 0:
-            pygame.draw.circle(screen, black, (centerX, centerY), 40, 2)
+            pygame.draw.circle(screen, BLACK, (centerX, centerY), 40, 2)
             circleCounter += 1
         else:
           centerX += 120
-          pygame.draw.circle(screen, black, (centerX, centerY), 40, 2)
+          pygame.draw.circle(screen, BLACK, (centerX, centerY), 40, 2)
           circleCounter += 1
         print(circleCounter)
       elif action == "Line":
@@ -71,31 +65,45 @@ def button(msg, x, y, w, h, ic, ac, action=None):
   screen.blit(textSurf, textRect)
 
 def text_objects(text, font):
-  textSurface = font.render(text, True, black)
+  textSurface = font.render(text, True, BLACK)
   return textSurface, textSurface.get_rect()
 
-pygame.init()
+def main():
+    pygame.display.set_caption('Finite Automata Generator')
+    screen.fill(BACKGROUND_COLOR)
 
-while running:
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      running = False
+    pygame.display.flip()
+    clock = pygame.time.Clock()
 
-  button("Circle", 150, 450, 100, 50, red, green, "Circle")
-  button("Line", 550, 450, 100, 50, red, green, "Line")
-  mouse = pygame.mouse.get_pos()
-  circleButton(mouse[0], mouse[1])
+    face = pygame.Surface((5, 5))
 
-  pygame.display.update()
-  clock.tick(15)
-  mouse = pygame.mouse.get_pos()
+    running = True
 
-  if 150+100 > mouse[0] > 150 and 450+50 > mouse[1] > 450:
-	pygame.draw.rect(screen, (200,0,0),(150,450,100,50))
-	if pygame.mouse.get_pressed():
-		pygame.draw.circle(screen, (0, 0, 0), (300, 50), 40, 2)
-  else:
-  	pygame.draw.rect(screen, (0,255,0),(150,450,100,50))
-  
-  pygame.display.update()
-  clock.tick(15)
+    pygame.init()
+
+    while running:
+      for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+          running = False
+
+      button("Circle", 150, 450, 100, 50, RED, GREEN, "Circle")
+      button("Line", 550, 450, 100, 50, RED, GREEN, "Line")
+      mouse = pygame.mouse.get_pos()
+      circleButton(mouse[0], mouse[1])
+
+      pygame.display.update()
+      clock.tick(15)
+      mouse = pygame.mouse.get_pos()
+
+      if 150+100 > mouse[0] > 150 and 450+50 > mouse[1] > 450:
+            pygame.draw.rect(screen, (200,0,0),(150,450,100,50))
+            if pygame.mouse.get_pressed():
+                    pygame.draw.circle(screen, (0, 0, 0), (300, 50), 40, 2)
+      else:
+            pygame.draw.rect(screen, (0,255,0),(150,450,100,50))
+      
+      pygame.display.update()
+      clock.tick(15)
+
+if __name__ == '__main__':
+    main()
